6634cc629739a2fd21c8e550f41888087766ae7f
Rait Kapp <3442567+raigu@users.noreply.github.com>
2022-04-28 11:09:21 +0300

    Display only asked questions

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -19,22 +19,18 @@ class QuestionRepository extends ServiceEntityRepository
         parent::__construct($registry, Question::class);
     }
 
-    // /**
-    //  * @return Question[] Returns an array of Question objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+      * @return Question[] Returns an array of Question objects
+      */
+    public function findAllAskedOrderedByNewest()
     {
         return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('q.askedAt IS NOT NULL')
+            ->orderBy('q.askedAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Question
